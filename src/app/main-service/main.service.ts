2ebf85132689c761260e59739f3c925d7176d5f8
mishel254 <sonimichie@gmail.com>
2020-10-04 21:07:20 +0300

    services

--- src/app/main-service/main.service.ts
+++ src/app/main-service/main.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient }  from '@angular/common/http';  
 import { Mainclass } from '../main-class/mainclass';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainService {
-
-  private _url:string ='https://api.github.com/users/daneden?access_token= + 817524962c7021c10455fbc8a92a4c866174a972';
+  private _url:string = environment.giturl + name + environment.apikey
 
   constructor(private http: HttpClient) { }
+ 
 
-  getData(){
-    return this.http.get<Mainclass[]>(this._url);
+  getData():Observable<any>{
+    return this.http.get<any>(this._url);
   }
+ 
 }
