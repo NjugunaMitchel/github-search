0ea951d504a59dbbfab8de5e527ba723f90f8fe1
mishel254 <sonimichie@gmail.com>
2020-10-04 21:06:04 +0300

    input data

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MainService } from 'src/app/main-service/main.service';
 import { Mainclass } from 'src/app/main-class/mainclass';
+ 
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -10,16 +12,23 @@ import { Mainclass } from 'src/app/main-class/mainclass';
 export class MainComponent implements OnInit {
 
   main:Mainclass;
+  name:string;
   
   constructor( private mainService:MainService) { }
-
-  ngOnInit()  {
-
-    
-     this.mainService.getData()
-      .subscribe(data => /* this.main = new Mainclass(Mainclass.username */ 
-        console.log(data))
+  ngOnInit(): void { }
+  
+  getUser(name){
+    Mainclass.username = name;
+    console.log(Mainclass.username)
+      
+      this.mainService.getData()
+      .subscribe(data =>
+        {
+          console.log(data)
+        })
+    }
+         
         
-  }
+      }
 
-}
+
