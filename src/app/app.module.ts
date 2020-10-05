b1223ac1819accd70cd062ae2fcb94a64541f4ba
mishel254 <sonimichie@gmail.com>
2020-10-05 11:00:27 +0300

    adding form Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,16 @@ import { MaterialModule } from './material/material.module';
 import { MainComponent } from './components/main/main.component';
 import { HttpClientModule }  from '@angular/common/http';
 import { MainService } from './main-service/main.service';
+import { FormsModule } from '@angular/forms';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    SearchComponent,
+    NavbarComponent
     
   ],
   imports: [
@@ -20,7 +25,8 @@ import { MainService } from './main-service/main.service';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [MainService],
   bootstrap: [AppComponent]
